a5163cae20cb629691887eb14273c9432c0253a5
bilal0075 <95081866+bilal0075@users.noreply.github.com>
2022-09-16 16:12:32 +0500

    changes

--- src/Components/_common/NavBar/MobileView/index.tsx
+++ src/Components/_common/NavBar/MobileView/index.tsx
@@ -1,5 +1,6 @@
 import { useWallet } from "@solana/wallet-adapter-react";
 import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+import { useEffect, useState } from "react";
 import { CgProfile } from "react-icons/cg";
 import { GiWallet } from "react-icons/gi";
 import { IoIosCreate } from "react-icons/io";
@@ -9,7 +10,16 @@ import styles from "../index.module.scss";
 
 const MobileView = () => {
   const { pathname } = useLocation();
-  const { wallet } = useWallet();
+  const { wallet, disconnect } = useWallet();
+  const [checkWallet, setCheckWallet] = useState<number | any>(null);
+  useEffect(() => {
+    if (wallet) {
+      setCheckWallet(null);
+    }
+  }, [wallet]);
+  const useDropdown = () => {
+    setCheckWallet(1);
+  };
   return (
     <div className={styles.mobileView}>
       <div className={styles.wrapper}>
@@ -38,12 +48,20 @@ const MobileView = () => {
           className={styles.card}
           form="walletButton"
           style={wallet ? { color: "green" } : { color: "white" }}
+          onClick={useDropdown}
         >
           <GiWallet />
-          <label>Wallet</label>
+          <label form="walletButton">Wallet</label>
           <div id="walletButton" style={{ display: "none" }}>
             <WalletMultiButton />
           </div>
+          {wallet == null || checkWallet == null ? (
+            ""
+          ) : (
+            <div className={styles.overFlowCard} onClick={disconnect}>
+              Disconnect
+            </div>
+          )}
         </label>
       </div>
     </div>
